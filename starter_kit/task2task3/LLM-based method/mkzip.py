27d1e5bd37d25d01dd57744dbc9155f9465419a9
Alex Rain <45069500+connectionRst@users.noreply.github.com>
2026-03-01 10:43:47 +0800

    commit some task23 code: - import notebook - split to 3 part - there is no accl as we use task parallel manipulate by CUDA_VISIBLE_DEVICES  :3 - add some support to gemma3, future work about qwen3 template is still needed - future work: cli args, proper multi model support, task23 val support, similar code refactor

---
+++ starter_kit/task2task3/LLM-based method/mkzip.py
@@ -0,0 +1,25 @@
+# %%
+import os
+
+#task config
+subtask = "subtask_2" # subtask_2 or subtask_3
+
+
+# %% [markdown]
+# ### Step 7: Save prediction results
+
+# %%
+# create zip
+
+# zip name is subtask folder name
+zip_name = f"{subtask}.zip"
+
+# zip the folder
+with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zf:
+    for root, _, files_in_dir in os.walk(subtask):
+        for file in files_in_dir:
+            full_path = os.path.join(root, file)
+            zf_path = os.path.relpath(full_path, ".")
+            zf.write(full_path, zf_path)
+
+
