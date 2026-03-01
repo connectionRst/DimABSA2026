23f06fbf564db7119b914888632beb35b0d6cc5a
Alex Rain <45069500+connectionRst@users.noreply.github.com>
2026-03-01 10:43:47 +0800

    refactor: to config in one file, merge the logic back & split some consts out

---
+++ starter_kit/task2task3/LLM-based method/mian.py
@@ -0,0 +1,198 @@
+from datasets import load_dataset
+from tqdm import tqdm
+
+import argparse
+import os
+
+import mytools
+
+os.environ["UNSLOTH_USE_MODELSCOPE"] = "1"
+PREFIX = mytools.prefix
+MODEL_TYPES = mytools.models
+DOMAIN_LANG = mytools.domain_lang
+
+# task config
+lr = 2e-4  # unsloth: use 2e-4 if the data is small
+model_type = "gemma-3"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('train_or_infer', default="infer")
+# change what domain you want
+parser.add_argument('domain', nargs='?', default="laptop")
+# change the language you want to test
+parser.add_argument('prd_lang', nargs='?', default="jpn")
+parser.add_argument('--model-type', default="gemma-3")
+# unsloth: use 2e-4 if the data is small
+parser.add_argument('--lr', type=float, default=2e-5)
+parser.add_argument('--task', type=int, default=2)
+parser.add_argument('--resume', type=bool, default=False)
+args = parser.parse_args()
+
+assert args.train_or_infer in ("train", "infer")
+assert args.task in (1, 2, 3)  # TODO support task1
+
+# you can change the model here
+if args.train_or_infer == "train":
+    if args.model_type == "gemma-3":
+        model_id = "unsloth/gemma-3-12b-it-unsloth-bnb-4bit"
+    elif args.model_type == "qwen-3":
+        model_id = "Qwen/Qwen3-4B-Instruct-2507"
+elif args.train_or_infer == "infer":
+    # TODO model id provider
+    model_id = f"./Lora/{args.model_type}/{args.task}_{args.domain}/model"
+else: raise ValueError("unsupport --model-type")
+
+def train(model_id, model_type, lr, task, domain, *, resume=False):
+    print(f"{model_id=}, {model_type=}, {lr=}, {task=}, {domain=}, {resume=}")
+
+    lora_ckpt_path = './Lora/{}/{}_{}'.format(model_type, task, domain)
+    train_urls = [f"{PREFIX}/subtask_{task}/{lang}/{lang}_{domain}_train_alltasks.jsonl" for lang in DOMAIN_LANG[domain]]
+
+    # load train data from url
+    dataset = load_dataset("json", data_files=train_urls)
+
+    # Display the data info
+    print(dataset)
+    print(dataset['train'][0]) # type: ignore
+
+    # ### Step 4: Load the LLM from Hugging Face and apply LoRA for fine-tuning
+    # tokenizer and model setting
+    model, tokenizer = mytools.get_model(model_type, model_id)
+
+    # covert dataset to train template
+    # yes, as tokenizer need to be loaded first, this cannot be raised top
+    train_dataset = dataset["train"].map(lambda x: mytools.to_task_prompt_mapper(x, task=task, tokenizer=tokenizer)) # type: ignore
+
+    # show your template text
+    print(train_dataset[100]["text"]) # type: ignore
+
+    # ### Step 5: Start LoRA Fine-Tuning
+    from trl import SFTTrainer, SFTConfig # pyright: ignore[reportPrivateImportUsage]
+
+    trainer = SFTTrainer(
+        model = model,
+        tokenizer = tokenizer, # pyright: ignore[reportCallIssue]
+        train_dataset = train_dataset,
+        eval_dataset = None, # Can set up evaluation!
+        args = SFTConfig(
+            dataset_text_field = "text",
+            per_device_train_batch_size = 2,
+            gradient_accumulation_steps = 4, # Use GA to mimic batch size!
+            warmup_steps = 5,
+            num_train_epochs = 2, #
+            learning_rate = lr, # Reduce to 2e-5 for long training runs
+            logging_steps = 1,
+            optim = "adamw_8bit",
+            weight_decay = 0.001,
+            lr_scheduler_type = "linear",
+            seed = 3407,
+            report_to = "none", # Use TrackIO/WandB etc
+            output_dir = f"./Lora/{model_type}/{task}_{domain}",
+            save_strategy = "steps",
+            save_steps = 100,
+        ),
+    )
+
+    #print(trainer.train_dataset[100]["input_ids"])
+
+    # TODO refractor this to mytools.py module
+    from unsloth.chat_templates import train_on_responses_only
+    if model_type in ("gemma-3"):
+        trainer = train_on_responses_only(
+            trainer,
+            instruction_part = "<start_of_turn>user\n",
+            response_part = "<start_of_turn>model\n",
+        )
+    else:
+        trainer = train_on_responses_only(
+            trainer,
+            instruction_part = "<|im_start|>user\n",
+            response_part = "<|im_start|>assistant\n",
+        )
+
+
+    trainer.train(resume_from_checkpoint=resume)
+
+    model.save_pretrained(lora_ckpt_path + "/model")  # Local saving
+    tokenizer.save_pretrained(lora_ckpt_path + "/model")
+
+def infer(model_id, model_type, task, domain, lang):
+    print(f"{model_id=}, {model_type=}, {task=}, {domain=}, {lang=}")
+    assert lang in DOMAIN_LANG[domain]
+    if task == 2: instruction = mytools.get_instruction_task2()
+    elif task == 3: instruction = mytools.get_instruction_task3()
+    else: raise ValueError("not supported task")
+
+    predict_url = f"{PREFIX}/subtask_{task}/{lang}/{lang}_{domain}_dev_task{task}.jsonl"
+
+    # load dev json to predict
+    predict_dataset = load_dataset("json", data_files=predict_url)
+
+    # Display the data info
+    print(predict_dataset["train"][0]) # type: ignore
+
+    model, tokenizer = mytools.get_model(model_type, model_id, fast_infer=True)
+
+    # ### Step 6: Run Inference and Extract Structured Sentiment Outputs
+
+    # Perform inference
+    results = []
+    for i, sample in enumerate(tqdm(predict_dataset["train"])):
+        text = sample["Text"]
+        final_prompt = instruction + '[Text] ' + text + '\n\nOutput:'
+        messages = mytools.wrap_prompt(final_prompt)
+
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize = True,
+            add_generation_prompt = True,
+            return_tensors = "pt",
+            return_dict = True
+        )
+
+        result = model.generate(
+            **text.to("cuda"),
+            max_new_tokens=1024,
+            temperature=0.7, top_p=0.8, top_k=20,
+        )
+
+        decoded = tokenizer.decode(result[0])  #FIXME output is None
+        extracted_text = decoded.split("\n")[-1]
+
+        key = "Triplet" if task == "task2" else "Quadruplet"
+
+        dump_data = {
+            "ID": sample.get("ID", f"sample_{i}"),
+            "Text": sample["Text"],
+            key: mytools.extract_answer(extracted_text, task),
+        }
+
+        tqdm.write(repr(dump_data))
+        results.append(dump_data)
+
+
+    # ### Step 7: Save prediction results
+    # TODO convert to def?
+    import json
+    import os
+
+    # resolve output name
+    out_name = f"pred_{lang}_{domain}.jsonl"
+
+    # ensure output folder
+    os.makedirs("subtask_{task}", exist_ok=True)
+
+    # JSONL file path
+    jsonl_path = os.path.join("subtask_{task}", out_name)
+
+    # write JSONL
+    with open(jsonl_path, "w", encoding="utf-8") as f:
+        for item in results:
+            f.write(json.dumps(item, ensure_ascii=False) + "\n")
+
+# main()
+if args.train_or_infer == "train":
+    train(model_id, args.model_type, args.lr, args.task, args.domain, resume=args.resume)
+elif args.train_or_infer == "infer":
+    infer(model_id, args.model_type, args.task, args.domain, args.prd_lang)
+else: raise ValueError("unsupport operation")
---
+++ starter_kit/task2task3/LLM-based method/mytools.py
@@ -0,0 +1,202 @@
+prefix = "../../../task-dataset/track_a"
+models = ("gemma-3", "qwen-3")
+moe_models = ("gemma-3")
+domain_lang = {
+    "laptop": ["eng", "zho"],
+    "restaurant": ["eng", "rus", "tat", "ukr", "zho"],
+    "hotel": ["jpn"]
+}
+
+def get_model(model_type, model_path, *, fast_infer=False):
+    '''
+    return (model w/ lora, tokenizer)
+    '''
+    if model_type in moe_models:
+        from unsloth import FastModel as FastLanguageModel
+    else:
+        from unsloth import FastLanguageModel
+    from unsloth.chat_templates import get_chat_template
+
+    # tokenizer and model setting
+    # TODO? for qwen3 load in 16bit
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = model_path,
+        max_seq_length = 512, # DimASBA Task usually less then 512 tokens.
+        # load_in_16bit = True,
+        # device_map = "balanced",
+        local_files_only = True
+    )
+
+    tokenizer = get_chat_template(
+        tokenizer,
+        chat_template = model_type,
+    )
+
+    if model_type == "gemma-3":
+        # lora setting
+        model = FastLanguageModel.get_peft_model(
+            model,
+            r = 8,
+            lora_alpha = 8,
+            lora_dropout = 0,
+            finetune_vision_layers = False, # Turn off for just text!
+        )
+    elif model_type == "qwen-3":
+        model = FastLanguageModel.get_peft_model(
+            model,
+            r = 8,
+            lora_alpha = 8,
+            lora_dropout = 0,
+        )
+    else: raise ValueError("invalid model_type", model_type)
+    if fast_infer: FastLanguageModel.for_inference(model)
+    return model, tokenizer
+
+def get_instruction_task2():
+    instruction = \
+    '''Below is an instruction describing a task, paired with an input that provides additional context. Your goal is to generate an output that correctly completes the task.
+
+    ### Instruction:
+    Given a textual instance [Text], extract all (A, O, VA) triplets, where:
+    - A is an Aspect term (a phrase describing an entity mentioned in [Text])
+    - O is an Opinion term
+    - VA is a Valence–Arousal score in the format (valence#arousal)
+
+    Valence ranges from 1 (negative) to 9 (positive),
+    Arousal ranges from 1 (calm) to 9 (excited).
+
+    ### Example:
+    Input:
+    [Text] average to good thai food, but terrible delivery.
+
+    Output:
+    [Triplet] (thai food, average to good, 6.75#6.38), (delivery, terrible, 2.88#6.62)
+
+    ### Question:
+    Now complete the following example:
+    Input:
+    '''
+    return instruction
+
+def get_instruction_task3():
+    rest_entity = 'RESTAURANT, FOOD, DRINKS, AMBIENCE, SERVICE, LOCATION'
+    rest_attribute = 'GENERAL, PRICES, QUALITY, STYLE_OPTIONS, MISCELLANEOUS'
+
+    laptop_entity = 'LAPTOP, DISPLAY, KEYBOARD, MOUSE, MOTHERBOARD, CPU, FANS_COOLING, PORTS, MEMORY, POWER_SUPPLY, OPTICAL_DRIVES, BATTERY, GRAPHICS, HARD_DISK, MULTIMEDIA_DEVICES, HARDWARE, SOFTWARE, OS, WARRANTY, SHIPPING, SUPPORT, COMPANY'
+    laptop_attribute = 'GENERAL, PRICE, QUALITY, DESIGN_FEATURES, OPERATION_PERFORMANCE, USABILITY, PORTABILITY, CONNECTIVITY, MISCELLANEOUS'
+
+    hotel_entity = 'HOTEL, ROOMS, FACILITIES, ROOM_AMENITIES, SERVICE, LOCATION, FOOD_DRINKS'
+    hotel_attribute = 'GENERAL, PRICE, COMFORT, CLEANLINESS, QUALITY, DESIGN_FEATURES, STYLE_OPTIONS, MISCELLANEOUS'
+
+    finance_entity = 'MARKET, COMPANY, BUSINESS, PRODUCT'
+    finance_attribute = 'GENERAL, SALES, PROFIT, AMOUNT, PRICE, COST'
+
+    entity_attribute_map = {
+        'restaurant': (rest_entity, rest_attribute),
+        'laptop': (laptop_entity, laptop_attribute),
+        'hotel': (hotel_entity, hotel_attribute),
+        'finance': (finance_entity, finance_attribute),
+    }
+
+    entity_label, attribute_label = entity_attribute_map[domain]
+
+    instruction = \
+    f'''Below is an instruction describing a task, paired with an input that provides additional context. Your goal is to generate an output that correctly completes the task.
+
+    ### Instruction:
+    Given a textual instance [Text], extract all (A, C, O, VA) quadruplets, where:
+    - A is an Aspect term (a phrase describing an entity mentioned in [Text])
+    - C is a Category label (e.g. FOOD#QUALITY)
+    - O is an Opinion term
+    - VA is a Valence–Arousal score in the format (valence#arousal)
+
+    Valence ranges from 1 (negative) to 9 (positive),
+    Arousal ranges from 1 (calm) to 9 (excited).
+
+    ### Label constraints:
+    [Entity Labels] ({entity_label})
+    [Attribute Labels] ({attribute_label})
+
+    ### Example:
+    Input:
+    [Text] average to good thai food, but terrible delivery.
+
+    Output:
+    [Quadruplet] (thai food, FOOD#QUALITY, average to good, 6.75#6.38),
+                (delivery, SERVICE#GENERAL, terrible, 2.88#6.62)
+
+    ### Question:
+    Now complete the following example:
+    Input:
+    '''
+    return instruction
+
+# extract answer
+def extract_answer(text, task):
+    import re
+    result = []
+    if task == 2:
+        pattern = r'\(([^,]+),\s*([^,]+),\s*([\d.]+#[\d.]+)\)'
+        matches = re.findall(pattern, text)
+
+        for aspect, opinion, va in matches:
+            meta_triplet = {}
+            meta_triplet["Aspect"] = aspect.strip()
+            meta_triplet["Opinion"] = opinion.strip()
+            meta_triplet["VA"] = va
+            result.append(meta_triplet)
+
+    elif task == 3:
+        pattern = r'\(([^,]+),\s*([^,]+),\s*([^,]+),\s*([^)]+)\)'
+        matches = re.findall(pattern, text)
+
+        for aspect, category, opinion, va in matches:
+            meta_quadra = {}
+            meta_quadra["Aspect"] = aspect.strip()
+            meta_quadra["Category"] = category.strip()
+            meta_quadra["Opinion"] = opinion.strip()
+            meta_quadra["VA"] = va
+            result.append(meta_quadra)
+    else:
+        raise ValueError("Invalid task")
+
+    return result
+
+# FIXME adjust for VL model?
+def wrap_prompt(prompt, answer=None):
+    prompt = [{
+            "role": "user",
+            "content": prompt
+        }]
+    if answer is not None:
+        prompt.append({"role": "assistant", "content": answer})
+    return prompt
+
+def to_task_prompt_mapper(x, *, task, tokenizer):
+    text = x["Text"]
+    quads = x.get("Quadruplet", [])
+    if task == 2:
+        answer = ", ".join([
+            f"({q['Aspect']}, {q['Opinion']}, {q['VA']})"
+            for q in quads
+        ])
+        instruction = get_instruction_task2()
+    elif task == 3:
+        answer = ", ".join((
+            f"({q['Aspect']}, {q['Category']}, {q['Opinion']}, {q['VA']})"
+            for q in quads
+        ))
+        instruction = get_instruction_task3()
+    else: raise ValueError("invalid task", task)
+    prompt = instruction + "[Text] " + text + "\n\nOutput:"
+    msg = wrap_prompt(prompt, answer)
+    try:
+        return {
+            "text":
+            tokenizer.apply_chat_template(
+                msg, tokenize=False, add_generation_prompt=False,
+                add_special_tokens=False
+            )
+        }
+    except:
+        return msg
